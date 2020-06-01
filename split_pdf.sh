be809167f184aad5053ca88eb9e8bfbaa397ff4f
Muzher Sharif <muzher@gmail.com>
2020-06-01 12:20:56 +0000

    split into 5 page chunks

--- split_pdf.sh
+++ split_pdf.sh
@@ -3,11 +3,11 @@ for pdf_file in ./pdf_dataset/**/*.pdf
 do
     FILENAME="$pdf_file"
     FILESIZE=$(stat -c%s "$FILENAME")
-    if [ $FILESIZE -gt 2000000 ]
+    if [ $FILESIZE -gt 1000000 ]
     then
         gs -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -q -o "$FILENAME.tmp" $FILENAME
         rm $FILENAME
-        qpdf --object-streams=generate --split-pages=10 "$FILENAME.tmp" ${FILENAME%.pdf}_%d.pdf
+        qpdf --object-streams=generate --split-pages=5 "$FILENAME.tmp" ${FILENAME%.pdf}_%d.pdf
         rm "$FILENAME.tmp"
     fi
 done
