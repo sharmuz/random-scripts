e8c8d007cd9793a8ce77bf3c9be5e8f54540fd32
Muz <43416513+sharmuz@users.noreply.github.com>
2025-04-01 09:38:54 +0100

    Save settings in user not workspace file

--- domino-vscode-start.sh
+++ domino-vscode-start.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 SETTINGS_DIR=${DOMINO_WORKING_DIR}/.vscode
+USER_SETTINGS_FILE=${SETTINGS_DIR}/User/settings.json
 
-# Add vscode user setting file only if it doesn't exist
+# Add vscode user settings file only if it doesn't exist
 # Add in DOMINO_WORKING_DIR so it persists across sessions
-if [ ! -f "${SETTINGS_DIR}/settings.json" ]; then
+if [ ! -f "$USER_SETTINGS_FILE" ]; then
 
 	sudo mkdir -p $SETTINGS_DIR
 	sudo chown -R domino:domino ${SETTINGS_DIR}
-	curl -fsSL -o ${SETTINGS_DIR}/settings.json https://raw.githubusercontent.com/sharmuz/random-scripts/refs/heads/master/vscode-settings.json
+	curl -fsSL -o $USER_SETTINGS_FILE https://raw.githubusercontent.com/sharmuz/random-scripts/refs/heads/master/vscode-settings.json
 
 fi
 
