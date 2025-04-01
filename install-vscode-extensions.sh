83123d7fbfa266e197cc0553e324f31b316cda68
Muz <43416513+sharmuz@users.noreply.github.com>
2025-04-01 15:56:30 +0100

    Add more extensions

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -3,10 +3,22 @@
 declare -a extensions
 
 extensions=(
+    # Python
     "python ms-python 2025.3.2025032601"
     "vscode-pylance ms-python 2025.3.103"
     "jupyter ms-toolsai 2025.2.0"
     "ruff charliermarsh 2025.22.0"
+    "autodocstring njpwerner 0.6.1"
+    # Git
+    "git-graph mhutchie 1.30.0"
+    "better-git-line-blame mk12 0.2.14"
+    # Markup, scripting and data
+    "rainbow-csv mechatroner 3.18.0"
+    "vscode-yaml redhat 1.17.0"
+    "even-better-toml tamasfe 0.21.2"
+    "shell-format foxundermoon 7.2.5"
+    "quarto quarto 1.119.0"
+    # Other
     "theme-dracula dracula-theme 2.25.1"
 )
 
